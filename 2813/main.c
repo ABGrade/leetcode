a70474da43c445948fe64f9a51fd12547e72f849
Batuev <batuev.arseniy@yandex.ru>
2025-02-03 07:02:57 +0300

    2813

---
+++ 2813/main.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int find_max_profit(int** items, int itemsSize)
+{
+    int ind_max = 0;
+    int max_profit = items[0][0];
+    for (int i = 1; i < itemsSize; i++)
+    {
+        if (max_profit < items[i][0])
+        {
+            max_profit = items[i][0];
+            ind_max = i;
+        }   
+    }
+    return ind_max;
+}
+
+int is_in(int *uniqs, int size, int elem)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (elem == uniqs[i])
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int calc_elegance(int **items, int *pairs, int k)
+{
+    int *uniqs = malloc(sizeof(int) * k);
+    int sum = items[pairs[0]][0];
+    uniqs[0] = items[pairs[0]][1];
+    int uniq_size = 1;
+
+    for (int i = 1; i < k; i++)
+    {
+        sum += items[pairs[i]][0];
+        if (!is_in(uniqs, uniq_size, items[pairs[i]][1]))
+        {
+            uniqs[uniq_size++] = items[pairs[i]][1];
+        }
+    }
+    
+    sum += uniq_size * uniq_size;
+    return sum;
+}
+
+
+int solve(int** items, int *pairs, int pairs_size, int itemsSize, int k)
+{
+    int max_elegance = calc_elegance(items, pairs, pairs_size);
+    if (pairs_size == k)
+    {
+        return max_elegance;
+    }
+    else
+    {
+        for (int i = 0; i < itemsSize; i++)
+        {
+            if (is_in(pairs, pairs_size, i))
+            {
+                continue;
+            }
+            pairs[pairs_size] = i;
+            int cur_elegance = solve(items, pairs, pairs_size + 1, itemsSize, k);
+            if (cur_elegance > max_elegance)
+            {
+                max_elegance = cur_elegance;
+            }
+        }
+    }
+    return max_elegance;
+}
+
+long long findMaximumElegance(int** items, int itemsSize, int k) 
+{
+    int *indexes = malloc(sizeof(int) * k);
+
+    long long max = find_max_profit(items, itemsSize);
+    indexes[0] = max;
+    
+    long long max_elegance = solve(items, indexes, 1, itemsSize, k);
+    return max_elegance;
+}
+
+int read_task_from_file(int ***items, int *item_size, int *k, char *filename)
+{
+    FILE *file = fopen(filename, "r");
+    if (fscanf(file, "%d", item_size) != 1)
+    {
+        return 1;
+    }
+    if (fscanf(file, "%d", k) != 1)
+    {
+        return 1;
+    }
+    *items = calloc(*item_size, sizeof(int *));
+    for (int i = 0; i < *item_size; i++)
+    {
+        (*items)[i] = malloc(sizeof(int) * 2);
+        for (int j = 0; j < 2; j++)
+        {
+            int num;
+            if (fscanf(file, "%d", &num) != 1)
+            {
+                return 1;
+            }
+            (*items)[i][j] = num;
+        }    
+    }
+    return 0;
+}
+
+void print_matrix(int **matrix, int rows)
+{
+    for (size_t i = 0; i < rows; i++)
+    {
+        printf("%d %d\n", matrix[i][0], matrix[i][1]);
+    }
+}
+
+int main()
+{
+    int **items = NULL;
+    int item_size = 0;
+    int k = 0;
+
+    if (read_task_from_file(&items, &item_size, &k, "matrix.txt"))
+    {
+        printf("Error\n");
+        return 1;
+    }
+    print_matrix(items, item_size);
+    
+    printf("%lld\n", findMaximumElegance(items, item_size, k));
+    return 0;
+}
